641edf89d43fb0fd504491ada02d4982288cd8b6
Omar Houmz <omarhoumz@gmail.com>
2018-02-21 18:03:22 +0000

    Adds for morocco in the logo

--- elts/navbar.php
+++ elts/navbar.php
@@ -1,7 +1,8 @@
 <nav class="navbar sticky-top navbar-dark navbar-expand-lg shadow">
 		<div class="container">
 			<a class="navbar-brand" href="/">
-				<img src="https://cdn-expa.aiesec.org/assets/images/aiesec-logo-white-blue.svg" width="180" alt="">
+				<img src="https://cdn-expa.aiesec.org/assets/images/aiesec-logo-white-blue.svg" width="180" alt="AIESEC Logo">
+				<div class="formorocco"><i>for Morocco</i></div>
 			</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
@@ -10,7 +11,7 @@
 				<ul class="navbar-nav">
 					<li class="nav-item active"><a href="#top" class="nav-link">Home</a></li>
 					<li class="nav-item"><a href="https://www.facebook.com/HumansOfAIESECOfficial/" target="_blank" class="nav-link">Blog</a></li>
-					<li class="nav-item"><a href="https://www.instagram.com/yalla_morocco/" class="nav-link" target="_blank">Yalla Morocco</a></li>
+					<li class="nav-item"><a href="https://yallamorocco.000webhostapp.com/" class="nav-link" target="_blank">Yalla Morocco</a></li>
 					<li class="nav-item">
 						<a href="https://www.aiesec.org/search" target="_blank" class="nav-link">More Opportunities</a>
 					</li>
